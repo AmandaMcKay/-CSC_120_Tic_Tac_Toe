95f74ffe2f873a24821c0df87f10ec0a09d33292
AmandaMcKay <akmckay@my.waketech.edu>
2021-12-06 18:13:26 -0500

    Stage 1: Printing Board

---
+++ board.py
@@ -0,0 +1,13 @@
+board = [["-", "-", "-"],
+         ["-", "-", "-"],
+         ["-", "-", "-"]]
+
+
+def print_board():
+    print("Printing Board...")
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+
+print_board()
